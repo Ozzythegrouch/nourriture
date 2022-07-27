f5c88f79af79564adcc0d9ce2ed80f2a560940ed
Anna D <adosev95@gmail.com>
2022-07-26 21:37:22 -0400

    add name component and intro aimations

--- foodProject/main.js
+++ foodProject/main.js
@@ -2,9 +2,24 @@
 const userInput = document.querySelector('.userInput');
 const message = document.querySelector('.message');
 const myButton = document.querySelector('.myButton');
+const content = document.querySelector('#contentContainer')
+const splashScreen = document.querySelector('.splash-screen')
+const messageFade = document.querySelector('#messageFade')
+const nameComp = document.querySelector('.nameComponent')
+
+function contentFade() {
+    setTimeout(()=>{
+        content.classList.add('contentContainerToFade')
+        content.classList.add('show')
+    }, 5000)
+}
 
 
 
 myButton.onclick = function() {
-    message.innerHTML= `Welcome to siteName, ${userInput.value}!`
+    messageFade.classList.add('messageFade')
+    nameComp.remove('nameComponent')
+    message.innerHTML= `Welcome, ${userInput.value}! Let's get cooking...`
+    contentFade()
 }
+
